863ed5a73b793334c97515e2f01a41c408984f46
Maruf Khan <maruf_kh01@yahoo.com>
2019-01-02 21:07:45 +0100

    import tools

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const express = require("express")
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
+app.use(morgan('dev'))
+app.use(cors())
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
 app.get('/',(req , res) => {
     res.json({
         message: 'Hello , welcome to my application'
@@ -9,4 +19,10 @@ app.get('/',(req , res) => {
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
+    mongoose.connect('mongodb://localhost:27017/management-app',
+    {useNewUrlParser: true}, 
+    () => {
+    console.log('Database connected...')
+    });
+
 })
